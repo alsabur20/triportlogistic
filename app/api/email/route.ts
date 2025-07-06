0fcb9f60bc2e507f351f1eb8a40beda300868846
Abdul Sabur <alsabur20@gmail.com>
2025-07-07 04:39:28 +0500

    changed to mail in email

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -24,7 +24,7 @@ export async function POST(request: NextRequest) {
 
     const mailOptions: Mail.Options = {
         from: process.env.MY_EMAIL,
-        to: process.env.MY_EMAIL,
+        to: process.env.TO_EMAIL,
         // cc: email, (uncomment this line if you want to send a copy to the sender)
         subject: subject,
         text: `Name: ${name} \nEmail: ${email} \nPhone: ${phone} \nMessage: ${message}`,
